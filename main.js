391ad8e838bb8868b6da06d02825c8b1704a1adb
istarlet <istarlet@gmail.com>
2022-03-28 00:00:41 -0400

    Add link to shuffle code origin

--- main.js
+++ main.js
@@ -1,22 +1,64 @@
-// let btn = document.querySelector(".btn");
-// let fortune = document.querySelector("#fortune");
-
-// function getFortune() {
-//   fortune.textContent = `You're going to have a wonderful day!`;
-// }
-
-// btn.addEventListener("click", getFortune);
-
 // Get the DOM elements
 let btn = document.querySelector(".btn");
 let fortune = document.querySelector(".fortune");
 
+// The fortune responses
+let responses = [
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes - definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful.",
+];
+
 /**
- * Get the fortune and render it into the DOM
+ * Randomly shuffle an array
+ * https://stackoverflow.com/a/2450976/1293256
+ * @param  {Array} array The array to shuffle
+ * @return {Array}       The shuffled array
  */
-function getFortune() {
-  fortune.textContent = `You're going to have a wonderful day!`;
+
+function shuffle(array) {
+  let currentIndex = array.length;
+  let temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// Create a new, shuffled array from lunch
+let shuffledFortunes = shuffle(Array.from(responses));
+
+// Get a random fortune from shuffled fortunes
+function getFortunes() {
+  fortune.textContent = `${shuffledFortunes[0]}`;
 }
 
 // Listen for clicks on the button
-btn.addEventListener("click", getFortune);
+btn.addEventListener("click", getFortunes);
